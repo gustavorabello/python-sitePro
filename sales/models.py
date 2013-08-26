f642080d030067e61ef08376dbaa32044d6dd627
Gustavo ANJOS <gustavo.rabello@gmail.com>
2013-08-26 14:22:40 -0400

    Added US moving furnitures for sale.

--- sales/models.py
+++ sales/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Sale(models.Model):
  obj = models.CharField(max_length=50)
  image = models.CharField(max_length=250)
+ cond = models.CharField(max_length=2000)
  link = models.CharField(max_length=2000)
  info = models.CharField(max_length=2000)
  original = models.CharField(max_length=20)
